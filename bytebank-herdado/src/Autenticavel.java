a2ab0d5e615e65ce87bcfb7f3228af9ec83c4319
gabrielcatanho <119543210+gabrielcatanho@users.noreply.github.com>
2023-05-12 12:50:10 -0300

    Add files via upload

---
+++ bytebank-herdado/src/Autenticavel.java
@@ -0,0 +1,12 @@
+
+//Contrato autenticavel
+	//quem assinar esse contrato, precisa implementar
+		//método setSenha
+		//método autentica
+
+public abstract interface Autenticavel {
+	
+	public abstract void setSenha(int senha);
+	
+	public abstract boolean autentica(int senha);
+}
